3b5cbc536a6fcb2a88498aded74fa6a04dfbe1dc
최원영 <sable@kangwon.ac.kr>
2022-08-29 14:25:59 +0000

    2447

--- problem/2447.py
+++ problem/2447.py
@@ -1,21 +1,6 @@
 n = int(input())
 
-def star(l):
-    if l == 3:
-        return ['***','* *','***']
+answer = ''
 
-    arr = star(l//3)
-    stars = []
-
-    for i in arr:
-        stars.append(i*3)
-
-    for i in arr:
-        stars.append(i+' '*(l//3)+i)
-
-    for i in arr:
-        stars.append(i*3)
-
-    return stars
-
-print('\n'.join(star(n)))
+def star():
+    pass
