594b7f2d44d62e42828057894e9f8ed5b3b7afef
최원영 <sable@kangwon.ac.kr>
2022-08-30 19:46:33 +0000

    add

--- problem/14502.py
+++ problem/14502.py
@@ -11,26 +11,31 @@ N , M = map(int, input().split()) #N -> 세로 길이 #M 가로 길이
 
 baseMap = [list(map(int,input().split())) for _ in range(N)]
 
-testMap = [[0 for _ in range(m)] for _ in range(n)]
-
+testMap = [[0 for _ in range(M)] for _ in range(N)]
 
 
 def bfs(): #검증
+    global answer
+    temp = 0 # 임시 answer 
     for n in range(N):
         for m in range(M):
-            testMap[x][y] = baseMap[x][y]
+            testMap[n][m] = baseMap[n][m]
+    for 
+    if temp > answer:
+        answer = temp
     
-            
 def bt(select): #backTracking
     if select == 3: 
+        bfs()
+         
         return;
     for n in range(N):
         for m in range(M):
-            if baseMap[n][m] == 1:
+            if baseMap[n][m] != 0:
                 continue
             baseMap[n][m] = 1
             bt(select + 1)
-            baseMap[][] = 0
+            baseMap[n][m] = 0
 
 
 # for x in range(n):
