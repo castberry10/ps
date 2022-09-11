73bc72edd000dba08c78c70f506a68044cbd0e53
최원영 <sable@kangwon.ac.kr>
2022-09-11 22:43:39 +0000

    으갹

--- problem/2xxxx/2630.py
+++ problem/2xxxx/2630.py
@@ -1,9 +1,47 @@
 n = int(input())
 paper = [list(map(int, input().split())) for _ in range(n)]
+whitepaper = 0
+bluepaper = 0
 
-def cntF():
-    ck = 1
-    for i in range(n):
-        for j in range(n):
-            if paper[0][0] != paper[i][j]:
+# 0 1 2 3 <- m
+
+# 1 1 0 0  0 <- n
+# 1 1 0 0  1
+# 0 0 0 0  2
+# 0 0 0 1  3
+
+# 0 0 4
+# > 0 0 2 / 0 2 2 / 2 0 2 / 2 2 2
+#
+# 2 2 2 
+# > 3 3 1 / 3 4 1 / 4 3 1 / 4 4 1
+def cntF(x, y, c): #초기는 0 0 4 이라 하면
+    global whitepaper
+    global bluepaper
+    # print(x, y, c)
+    ck = True
+    for i in range(y, y + c):
+        for j in range(x, x + c):
+            if paper[x][y] != paper[j][i]:
                 
+                
+                ck = False
+                c2 = c // 2
+                cntF(x, y, c2)
+                cntF(x + c2, y, c2)
+                cntF(x, y + c2, c2)
+                cntF(x + c2, y + c2, c2)
+                return
+        if not ck:
+            break
+    if ck:
+        color = paper[x][y] #사실 y x긴 한데 
+        if color == 0:
+            whitepaper += 1
+        else:
+            bluepaper += 1
+    return
+cntF(0, 0, n)
+
+print(whitepaper)
+print(bluepaper)
--- problem/5615.py
+++ problem/5615.py
@@ -14,54 +14,33 @@ def fastPowMod(a,b,m):    # a^b mod m
 
     return result
 
-def primeCheck(n):
-    primeN = True
-    
-    if n > 2:
-        if fastPowMod(2, n, n) == 2:
-            pass
-        else:
-            # print(2, fastPowMod(2, n, n))
-            primeN = False
-            return primeN
-    if n > 3:
-        if fastPowMod(3, n, n) == 3:
-            pass
-        else:
-            # print(3, fastPowMod(3, n, n))
-            primeN = False
-            return primeN
-    
-    if n > 7:
-        if fastPowMod(7, n, n) == 7:
-            pass
-        else:
-            # print(7, fastPowMod(7, n, n))
-            primeN = False
-            return primeN
-    
-    if n > 61:
-        if fastPowMod(61, n, n) == 61:
-            pass
-        else:
-            # print(61, fastPowMod(61, n, n))
-            primeN = False
-            return primeN
-    
-    
-    return primeN
+def miller_rabin(n, a):
+    r = 0
+    d = n-1
+    while (d % 2 == 0):
+        r += 1
+        d = d // 2
+    x = fastPowMod(a, d, n)
+    if x == 1 or x == n-1:
+        return True
+    for i in range(0,r-1):
+        x = fastPowMod(x, 2, n)
+        if x == n-1:
+            return True
+    return False
 
-# for i in range(n):
-#     S = sys.stdin.readline().rstrip()
-#     S = int(S)
-#     ses = 2 * S + 1 
-#     for i in range 
-#     # if primeCheck(ses):
-#         # print(primeCheck(ses), ses)
-#         answer += 1
-        
-for i in range(2, 100000):
-    if primeCheck(i):
-        print(i)
+
+for i in range(n):
+    S = sys.stdin.readline().rstrip()
+    S = int(S)
+    ses = 2 * S + 1 
+    # checklist = [2, 7, 61] # 42억아래 결정적 뭐시기 <- ? 
+    checklist = [2, 3, 5, 7, 11, 61]
+    flag = True
+    for i in checklist:
+        if not miller_rabin(ses, i):
+            flag = False
+            break
+    if flag:
         answer += 1
 print(answer)
