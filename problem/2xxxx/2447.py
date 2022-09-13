c4b74ae99bd4b8fde0e1f81323ac0aa59a2061bf
최원영 <sable@kangwon.ac.kr>
2022-09-13 12:18:54 +0000

    commit

--- problem/2xxxx/2447.py
+++ problem/2xxxx/2447.py
@@ -2,5 +2,15 @@ n = int(input())
 
 answer = ''
 
-def star():
-    pass
+def star(n):
+    global answer
+    # if n == 3:
+    #     # answer += '***\n'
+    #     # answer += '* *\n'
+    #     # answer += '***\n'
+    #     ans
+    if n == 1:
+        answer += '*'
+    
+    if 
+        
