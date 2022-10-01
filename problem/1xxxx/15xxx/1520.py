ec41c8d9daaac3eed36fb71a7cdd81c8916cf790
최원영 <sable@kangwon.ac.kr>
2022-10-01 14:48:39 +0000

    add

--- problem/1xxxx/15xxx/1520.py
+++ problem/1xxxx/15xxx/1520.py
@@ -6,17 +6,21 @@ dxy = [(-1, 0), (1, 0), (0, -1), (0 , 1)]
 
 def dp(y, x):
     if dpmap[y][x] != 0: #이미 데이터가 있다면  > ? 
-        pass
+        # pass
+        return dpmap[y][x]
     if y == N - 1 and x == M - 1: #오른쪽 끝 설정
-        dpmap[y][x] = 1 
-        
+        # dpmap[y][x] = 1 
+        return 1
     for i in range(4):
         dx, dy = dxy[i]
         dxx = dx + x
         dyy = dy + y
-        if 0 <= dxx < M and 0 <= dyy < N: #인덱스 에러로부터 안田전
-            pass
-
+        if 0 <= dxx < M and 0 <= dyy < N: #인덱스 에러로부터 안전
+            print(1)
+            if datamap[y][x] < datamap[dyy][dxx]:
+                print(0)
+                dpmap[y][x] += dp(dyy, dxx)
+    return dpmap[y][x]
 dp(0, 0 )
-
+print(dpmap)
 print(dpmap[0][0]) #왼쪽 끝
