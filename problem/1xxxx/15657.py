e74b37347a4353e7d9cb9f4f69176e7e6d738d2b
최원영 <sable@kangwon.ac.kr>
2022-10-14 14:47:37 +0000

    add

---
+++ problem/1xxxx/15657.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+A = list(map(int , input().split()))
+A.sort()
+#N에서 M만ㅋㅡㅁ 뽑는다 
+answer = []
+answerlist = set()
+def bt(cnt):
+    # 2 2  > 
+    # 1 1 1 2 2 1 2 2 > 
+    if cnt  == M:
+        c1 = len(answerlist)
+        a = ''
+        for i in answer:
+        	a += str(i) + ' '
+        
+        answerlist.add(a)
+        c2 = len(answerlist)
+        if c1 == c2:
+            return
+        print(a)
+        return
+	    
+    for i in A:
+        answer.append(i)
+        bt(cnt + 1)
+        answer.pop()
+bt(0)
+
+#계수정렬 하거나 할듯  
