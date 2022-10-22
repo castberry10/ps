ce505e00608d91f30145f737977e6c6d873ffacf
최원영 <sable@kangwon.ac.kr>
2022-10-22 14:38:54 +0000

    add

---
+++ problem/jiwon-help/202210/Ball22.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+ 
+public class Ball22{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String dataline;
+		while(sc.hasNextLine()) {
+			dataline = sc.nextLine();
+			String[] slicedata = dataline.split(" ");
+			int N = Integer.parseInt(slicedata[0]);
+			int K = Integer.parseInt(slicedata[1]);
+			
+			
+		}
+	}
+	
+	void bt(int n, int k){
+		
+	}
+}
---
+++ problem/jiwon-help/202210/README.md
@@ -0,0 +1,5 @@
+# Ball22
+[img](https://cdn.discordapp.com/attachments/920404428706680832/1033278289785135194/unknown.png)
+
+# Ant22
+[img](https://cdn.discordapp.com/attachments/920404428706680832/1033278376791781426/unknown.png)
