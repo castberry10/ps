f9caceb314020009d47123848ff281d03e8977d5
최원영 <sable@kangwon.ac.kr>
2022-11-02 03:21:41 +0000

    add

--- problem/1xxxx/11xxx/1100.py
+++ problem/1xxxx/11xxx/1100.py
@@ -1,10 +1,10 @@
 borad = list()
 for i in range(8):
-    borad.append(input().split(""))
+    borad.append(list(input()))
 cnt = 0
 for i in range(8):
     for j in range(8):
         if borad[i][j] == 'F':
-            if (i % 2 == 0 and j % 2 == 1) or (i % 2 == 1 and j % 2 == 0):
+            if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 ==1):
                 cnt += 1
 print(cnt)
---
+++ problem/2xxxx/23235.py
@@ -0,0 +1,11 @@
+cnt = 1
+while True:
+    a = input()
+    if a == '0':
+        break
+    # b = list(map(int, input().split()))
+    # c = b.sort()
+    # if b == c:
+    print('Case ' + str(cnt) +': Sorting... done!')
+    cnt += 1
+    
