2ff5ddaf1c67928024377b327b6b56b6ac3b3115
최원영 <sable@kangwon.ac.kr>
2022-11-17 09:38:44 +0000

    ad

---
+++ problem/4153.py
@@ -0,0 +1,10 @@
+while True:
+    l = list(map(int, input().split()))
+    l.sort()
+    
+    if l[0] == l[1] == l[2] == 0:
+        break
+    if l[0] * l[0] + l[1] * l[1] == l[2] * l[2]:
+        print('right')
+    else:
+        print('wrong')
