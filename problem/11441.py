4bc473a11a8695fc22a4e39cc6b9290912c606c8
최원영 <sable@kangwon.ac.kr>
2023-01-15 22:34:34 +0000

    add

---
+++ problem/11441.py
@@ -0,0 +1,18 @@
+import sys
+n = int(input())
+data = list(map(int,input().split()))
+m = int(input())
+prefixSumList = [0] * n
+prefixSumList[0] = data[0]
+for i in range(1, n):
+    prefixSumList[i] += data[i] + prefixSumList[i - 1]
+print(prefixSumList)
+for _ in range(m):
+    i1, i2 = map(int, sys.stdin.readline().split())
+    i1, i2 = i1 - 1, i2 - 1
+    if i1 == 0:
+        print(prefixSumList[i2])
+    else:
+        print(prefixSumList[i2] - prefixSumList[i1 - 1])
+        
+    
