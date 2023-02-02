5ca3714d05f61a1fde5037abe632fa4ae876aa0d
최원영 <sable@kangwon.ac.kr>
2023-02-02 11:50:08 +0000

    add

--- problem/temp.py
+++ problem/temp.py
@@ -1,8 +1,18 @@
-cnt = 0
-while True:
-    try:
-        a = input()
-        cnt += 1
-    except:
-        print(cnt)
-        break
+a, b = map(int, input().split())
+
+def print280():
+    print(280)
+
+def print320():
+    print(320)
+
+if a < 12:
+    print280()
+    
+elif a < 17:
+    if b == 1:
+        print280()
+    else:
+        print320()
+else:
+    print280()
