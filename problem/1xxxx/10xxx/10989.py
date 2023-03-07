bd6c16ec9e768a03b199f02fc711af6041dcba7b
최원영 <sable@kangwon.ac.kr>
2023-03-07 12:25:43 +0000

    set

--- problem/1xxxx/10xxx/10989.py
+++ problem/1xxxx/10xxx/10989.py
@@ -1,11 +1,12 @@
 import sys
-N = int(input())
-sortls = [0] * 10002
 
-for i in range(N):
-    a = int(sys.stdin.readline())
-    sortls[a] += 1
+n = int(sys.stdin.readline())
+nlist = [0] * 10001
 
-for i in range(1,N + 1):
-    for _ in range(sortls[i]):
-        print(i)
+for _ in range(n):
+    nlist[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if nlist[i] != 0:
+        for _ in range(nlist[i]):
+            print(i)
