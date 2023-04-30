4abb3782e764cb4f5809d18cf683a50ca999502f
최원영 <sable@kangwon.ac.kr>
2023-04-30 09:33:52 +0000

    add

--- problem/temp.py
+++ problem/temp.py
@@ -1,15 +1,1 @@
-cnt = 0
-a = int(input())
-
-while True:
-    if a < 10:
-        print(cnt)
-        break
-    a = str(a)
-    sum = 1
-    cnt += 1
-    for c in a:
-        sum *= int(c)
-        
-    a = int(sum)
-    
+print(chr(99), chr(97),chr(115),chr(116),chr(98),chr(101),chr(114),chr(114),chr(121),sep = "")
