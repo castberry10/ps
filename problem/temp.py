748d49ac1c6db30f1b8669c40cc2ed8d0c8e5430
최원영 <sable@kangwon.ac.kr>
2023-09-19 15:26:06 +0000

    set

--- problem/temp.py
+++ problem/temp.py
@@ -1,7 +1,6 @@
-a = 12345
-print(a)
-print(str(a))
-print(map(int, str(a)))
-print(sum(map(int, str(a))))
-a1, a2, a3, a4, a5 = map(int, str(a))
-print(a1, a2, a3, a4, a5)
+x1 = int(input("x1: "))
+y1 = int(input("y1: "))
+x2 = int(input("x2: "))
+y2 = int(input("y2: "))
+
+print("두 점 사이의 거리=", ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** (1/2) )
