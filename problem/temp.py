edc51c2790e9688b0e7794f5b430bbd006db7f66
최원영 <sable@kangwon.ac.kr>
2023-09-20 15:51:50 +0000

    set

--- problem/temp.py
+++ problem/temp.py
@@ -1,6 +1,6 @@
-x1 = int(input("x1: "))
-y1 = int(input("y1: "))
-x2 = int(input("x2: "))
-y2 = int(input("y2: "))
+a, b = map(int, input().split())
 
-print("두 점 사이의 거리=", ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** (1/2) )
+if a > b:
+    print(2*b+1)
+else:
+    print((a-1) * 2 + 1 )
