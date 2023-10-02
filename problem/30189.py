90a7d59f46525c360a368dd0a560732a768a01e8
최원영 <sable@kangwon.ac.kr>
2023-10-02 06:33:32 +0000

    add

---
+++ problem/30189.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+# ans = 0
+# ansset = set()
+# for i in range(n+1):
+#     for j in range(m+1):
+#         ansset.add((i, j))
+# print(len(ansset))
+
+print((n +1) * (m + 1))
