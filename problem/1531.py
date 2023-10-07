2f29fd64e6ea6f8ef53ec288783a52734ade4cde
최원영 <sable@kangwon.ac.kr>
2023-10-07 07:26:46 +0000

    add

--- problem/1531.py
+++ problem/1531.py
@@ -1,10 +1,17 @@
-n = int(input())
+n, m = map(int, input().split())
 
-data = [[1 for _ in range(100)] for _ in range(100)]
-print(data)
-for i in range(n):
+data = [[m+1 for _ in range(100)] for _ in range(100)]
+answer = 0
+# print(data)
+for _ in range(n):
     x1, y1, x2, y2 = map(int, input().split())
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            data[j][i] = 0
-pritn(sum(data))
+    for i in range(x1-1, x2):
+        for j in range(y1-1, y2):
+            data[j][i] -= 1
+
+for i in range(100):
+    for j in range(100):
+        if data[j][i] <= 0:
+            answer += 1
+print(answer)
+
