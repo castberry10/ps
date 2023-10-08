2927bac4ec30f7789983f06033bf69dc8f059adf
최원영 <sable@kangwon.ac.kr>
2023-10-08 06:32:07 +0000

    add

---
+++ problem/2161.py
@@ -0,0 +1,25 @@
+from collections import deque
+# deque.append(item): item을 데크의 오른쪽 끝에 삽입한다.
+# deque.appendleft(item): item을 데크의 왼쪽 끝에 삽입한다.
+# deque.pop(): 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.popleft(): 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.extend(array): 주어진 배열(array)을 순환하면서 데크의 오른쪽에 추가한다.
+# deque.extendleft(array): 주어진 배열(array)을 순환하면서 데크의 왼쪽에 추가한다.
+# deque.remove(item): item을 데크에서 찾아 삭제한다.
+# deque.rotate(num): 데크를 num만큼 회전한다(양수면 오른쪽, 음수면 왼쪽).
+
+#--
+#좌측이 위
+n = int(input())
+data = list(range(1, n+1))
+deq = deque(data)
+answer = list()
+while True:
+    if len(deq) == 1:
+        answer.append(deq.pop())
+        break
+    else:
+        answer.append(deq.popleft())
+        deq.append(deq.popleft())
+    # print()
+print(*answer)
