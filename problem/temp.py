dbbd3d73fc452df7e9dea121ea99378701f1ce68
최원영 <sable@kangwon.ac.kr>
2023-11-09 10:57:49 +0000

    add

--- problem/temp.py
+++ problem/temp.py
@@ -1,21 +1,21 @@
-data = dict()
-while True:
-    n = int(input('버킷 리스트 메뉴 선택 (1.추가 2.삭제 3.보기 0.종료):'))
-    if n == 1:
-        k, v = input('사전에 추가할 단어를 입력하세요: ').split()
-        data[k] = v
-        print(data)
-    elif n == 2:
-        if len(data) == 0:
-            print('사전에 아무것도 없습니다!')
-        else:
-            s = input('삭제할 단어를 입력하세요: ')
-            del data[s]
-            print(data)
-    elif n == 3:
-        print(data)
-    elif n == 0:
-        print('프로그램을 종료합니다.')
-        break
-    else:
-        print('메뉴 선택 오류입니다. 다시 선택하세요')
+n, l = map(int, input().split())
+maxline = 0
+maxlinecnt = 0
+
+for i in range(n):
+    precolor = ""
+    templine = 0
+    data = input()
+    for j in range(l):
+        if precolor != "1" and data[j] == '1':
+            precolor = "1"
+            templine += 1
+        if data[j] == '0':
+            precolor = '0'
+    if templine > maxline:
+        maxlinecnt = 1
+        maxline = templine
+    elif templine == maxline:
+        maxlinecnt += 1
+    # print(templine)
+print(maxline, maxlinecnt)
