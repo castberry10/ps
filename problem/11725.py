2b572570f6c9f279320096ec282facd6d613ff4b
wychoi <soc09072@gmail.com>
2024-10-17 14:11:23 +0000

    트리의 부모

--- problem/11725.py
+++ problem/11725.py
@@ -1,15 +1,31 @@
 from collections import deque 
+import sys
+
+input = sys.stdin.readline
 n = int(input())
 tree = {}
 
 answer = [0] * (n + 1)
-for i in range(n):
+for i in range(n - 1):
     a, b = map(int, input().split())
-    tree[a] = b
-    tree[b] = a
+    if a in tree:
+        tree[a].append(b)
+    else:
+        tree[a] = [b]
+    if b in tree:
+        tree[b].append(a)
+    else:
+        tree[b] = [a]
 queue = deque()
 
 queue.append(1)
 
 while queue:
-    
+    current_node = queue.popleft() # 현재 노드
+
+    for i in tree[current_node]:
+        if answer[i] == 0:
+            answer[i] = current_node
+            queue.append(i)
+for i in range(2, n + 1):
+    print(answer[i])
