f1216d97c31f1f1cfe8eaa64e82b5d08e6c932d2
wychoi <soc09072@gmail.com>
2024-10-27 11:31:35 +0000

    파일정리

--- problem/1xxxx/12xxx/1260.py
+++ problem/1xxxx/12xxx/1260.py
@@ -1,13 +1,51 @@
-s1 = {}
-s2 = {}
-N, M = map(int, input().split())
-for i in range(N):
-    s1[i + 1] = input()
+import sys
+from collections import deque
 
-s2 = {v:k for k,v in s1.items()}
-for i in range(M):
-    a = input()
-    if a.isnumeric():
-        print(s1[int(a)])
-    else:
-        print(s2[a])
+input = sys.stdin.readline
+n, m, v = map(int, input().split())
+graph = {}
+
+answerDFS = []
+answerBFS = []
+
+for i in range(m):
+    a, b = map(int, input().split())
+    if a not in graph:
+        graph[a] = []
+    if b not in graph:
+        graph[b] = []
+    graph[a].append(b)
+    graph[b].append(a)
+for l in graph:
+    graph[l].sort()
+
+def dfs(n):
+    answerDFS.append(n)
+    if n not in graph:
+        return
+    for i in graph[n]:
+        if i not in answerDFS:
+            dfs(i)
+    
+def bfs():
+    global n, m, v
+    visit = set()
+    queue = deque()
+    queue.append(v)
+    visit.add(v)
+    while queue:
+        temp = queue.popleft()
+        answerBFS.append(temp)
+        if temp not in graph:
+            continue 
+        for i in graph[temp]:
+            if i not in visit: 
+                queue.append(i)
+                visit.add(i)
+    print(*answerBFS)
+
+
+dfs(v)
+print(*answerDFS)
+bfs()
+
