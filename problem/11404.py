1debeb0af04c390b3492610de8f00153d085bba5
wychoi <soc09072@gmail.com>
2024-11-13 13:55:25 +0000

    플로이드-워셜 알고리즘을 사용하여 최단 경로 계산 기능 추가

---
+++ problem/11404.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+graph = [[1e11 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for i in range(n):
+    graph[i + 1][i + 1] = 0
+
+for i in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][k] < 1e11 and graph[k][j] < 1e11:
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+for i in range(n):
+    for j in range(n):
+        if graph[i + 1][j + 1] >= 1e11:
+            print(0, end = " ")
+        else:
+            print(graph[i+1][j+1] , end = " ")
+    if i != n - 1:
+        print()
+
