7735ec095ca127cce6d87ddaadca0cee1e979cfe
wychoi <soc09072@gmail.com>
2025-01-14 01:00:23 +0000

    주석 추가: 혈액형 조합 알고리즘 및 KAIST HAJE25-0107 문제의 입력 처리 개선

--- problem/codeup/2322.py
+++ problem/codeup/2322.py
@@ -1,6 +1,6 @@
 a, b = input().split()
-answer = set() # set 혈액형 중복 방지
-answer2 = set()
+answer = set() # set 혈액형 중복 방지 # AA
+answer2 = set() # BB
 dicti = {'AA':'A', 'AO':'A', 'BB' : 'B', 'BO': 'B', 'AB': 'AB', 'OO': 'O'} # 맵핑
 # 혈액형 조합 
 for i in range(2):
@@ -8,9 +8,11 @@ for i in range(2):
         temp = []
         temp.append(a[i])
         temp.append(b[j])
+        # temp = [A, B] [A, A]
         temp.sort() # 혈액형 인자 모음
         strtemp = ''
         strtemp += temp[0] + temp[1]
+        # strtemp 'AB'  'AA'
         answer.add(strtemp)
         
 for i in answer:
--- problem/knu/AlLerGy/KAIST HAJE25-0107/D.py
+++ problem/knu/AlLerGy/KAIST HAJE25-0107/D.py
@@ -1,3 +1,4 @@
+# 32353
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -5,3 +6,5 @@ h, w, r1, c1, r2, c2 = map(int, input.split())
 mapdata = []
 for i in range(h):
     mapdata.append(list(map(int, input().split())))
+for i in range(w):
+    p, q = int(input())
