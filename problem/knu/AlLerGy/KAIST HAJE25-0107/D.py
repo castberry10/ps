d28e929d216f5ce5635e62a0b78c5ee33243b523
wychoi <soc09072@gmail.com>
2025-01-14 05:40:30 +0000

    입력 처리 개선: 사과 좌표를 리스트에 추가하고 입력 방식을 수정

--- problem/knu/AlLerGy/KAIST HAJE25-0107/D.py
+++ problem/knu/AlLerGy/KAIST HAJE25-0107/D.py
@@ -3,8 +3,25 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 h, w, r1, c1, r2, c2 = map(int, input.split())
+apple_list = list()
 mapdata = []
 for i in range(h):
     mapdata.append(list(map(int, input().split())))
 for i in range(w):
-    p, q = int(input())
+    p, q = map(int, input().split())
+    apple_list.append([p,q])
+
+# 위 1 오른쪽 2 아래 3 왼쪽 4 
+if r2-r1 == 0:
+    if c2 - c1 == -1:
+        pass
+    else:
+        pass 
+else:
+    if r2 - r1 == -1:
+        pass
+    else:
+        pass
+state = 1
+while True:
+    
