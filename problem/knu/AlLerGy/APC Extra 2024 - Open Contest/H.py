8d18e30602d7c54823d18d78d943c2eea6aacde1
wychoi <soc09072@gmail.com>
2025-01-18 09:28:38 +0000

    입력 처리 개선: 그래프 초기화 방식 수정 및 BFS 알고리즘 최적화

--- problem/knu/AlLerGy/APC Extra 2024 - Open Contest/H.py
+++ problem/knu/AlLerGy/APC Extra 2024 - Open Contest/H.py
@@ -1,43 +1,34 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
-n = int(input())
-graph = {}
-fnlist = 0
-for i in range(n-1):
+n = int(input().strip())
+
+graph = [[] for _ in range(n)]
+for _ in range(n-1):
     u, v = map(int, input().split())
-    if u not in graph:
-        graph[u] = []
-    if v not in graph:
-        graph[v] = []
     graph[u].append(v)
     graph[v].append(u)
-    
-def bfs(n):
-    global fnlist
-    q = deque()
-    visit = set()
-    visit.add(0)
-    visit.add(n)
-    q.append(0)
-    while q:
-        currentNode = q.popleft()
-        if currentNode in graph:
-            for nextNode in graph[currentNode]:
-                if nextNode not in visit:
-                    visit.add(nextNode)
-                    q.append(nextNode)
-    visit.remove(n)
-    visit.remove(0)
-    temp = 0
-    for i in visit:
-        temp += 2 ** (i-1)
-    fnlist ^= temp
 
-for i in range(n-1):
-    bfs(i+1)
-    
-binary = bin(fnlist)[2:]
-if len(binary) < n-1: # 2 < 4 
-    binary = ('0' * ((n - 1) - len(binary))) +binary
-print(binary)
+dist = [-1] * n
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    dist[start] = 0
+    while q:
+        cur = q.popleft()
+        for nxt in graph[cur]:
+            if dist[nxt] == -1:
+                dist[nxt] = dist[cur] + 1
+                q.append(nxt)
+
+bfs(0)
+answer_bits = []
+for v in range(1, n):
+    bit = ((n-1) - dist[v]) % 2
+    answer_bits.append(str(bit))
+
+answer_bits.reverse()
+
+print("".join(answer_bits))
