812070a59f4592e2b4a3960b774ced931b2e1a87
wychoi <soc09072@gmail.com>
2025-03-17 02:56:17 +0000

    파일 삭제: 여러 문제 관련 파일 및 코드 삭제

--- problem/jiwon-help/202210/README.md
+++
@@ -1,5 +0,0 @@
-# Ball22
-![img](https://cdn.discordapp.com/attachments/920404428706680832/1033278289785135194/unknown.png)
-
-# Ant22
-![img](https://cdn.discordapp.com/attachments/920404428706680832/1033278376791781426/unknown.png)
--- problem/knu/opensw/w9/1.py
+++
@@ -1,9 +0,0 @@
-def cardsum(cards, n):
-    for i in range(len(cards)):
-        for j in range(i+1, len(cards)):
-            if cards[i] + cards[j] == n:
-                return (i, j)
-    return (-1, -1)
-print(cardsum([1, 2, 3, 4, 5], 8))  # 12
-print(cardsum([1, 2, 3, 1, 2, 3], 2))
-print(cardsum([1, 2, 3], 6)) 
